8f8dd975614245edaaf6d3ea96e9db1791bf3656
Matve1q300 <popovmatvei33@gmail.com>
2025-07-27 22:27:57 +0530

    message

--- src/main/java/com/tmelement/TMElementCore.java
+++ src/main/java/com/tmelement/TMElementCore.java
@@ -42,8 +42,7 @@ public class TMElementCore {
     public void postInit(FMLPostInitializationEvent event) {
         proxy.postInit(event);
     }
-
-
+    //sosal?  git config --global user.email "you@example.com"
 }
 
 
